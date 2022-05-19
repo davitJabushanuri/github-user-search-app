7b9104db4bd0f3fea7fc2de322a535e0accf2870
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-20 02:11:40 +0400

    add theme state and changeTheme function that will handle theme swithing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
 import './scss/main.scss'
 import Header from './components/Header'
 import Card from './components/Card'
 
 function App() {
+	const [theme, setTheme] = useState('LIGHT')
+
 	return (
 		<main className='App light'>
-			<Header />
+			<Header theme={theme} setTheme={setTheme} />
 			<Card />
 		</main>
 	)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import DarkThemIcon from '../assets/icon-moon.svg'
 import LightThemIcon from '../assets/icon-sun.svg'
 
-const Header = () => {
+const Header = ({ theme, setTheme }) => {
+	const changeTheme = e => {
+		setTheme(theme === 'LIGHT' ? 'DARK' : 'LIGHT')
+	}
+
 	return (
 		<section className='header'>
 			<h1 className='header__h1'>devfinder</h1>
-			<div className='header__theme'>
-				<h2 className='header__theme__h2'>DARK</h2>
+			<button onClick={e => changeTheme(e)} className='header__theme'>
+				<h2 className='header__theme__h2'>
+					{theme === 'LIGHT' ? 'DARK' : 'LIGHT'}
+				</h2>
 				<img src={DarkThemIcon} alt='' className='header__theme__icon' />
-			</div>
+			</button>
 		</section>
 	)
 }
