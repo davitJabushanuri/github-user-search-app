95632651b9bffde5f0f662669cd434df7617900a
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-20 17:22:48 +0400

    style stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const Stats = () => {
-	return <div>Stats</div>
+	return (
+		<div className='stats'>
+			<div className='stats__flex'>
+				<h4 className='stats__flex__header'>Repos</h4>
+				<h3 className='stats__flex__number'>8</h3>
+			</div>
+			<div className='stats__flex'>
+				<h4 className='stats__flex__header'>Followers</h4>
+				<h3 className='stats__flex__number'>3938</h3>
+			</div>
+
+			<div className='stats__flex'>
+				<h4 className='stats__flex__header'>Following</h4>
+				<h3 className='stats__flex__number'>9</h3>
+			</div>
+		</div>
+	)
 }
 
 export default Stats
