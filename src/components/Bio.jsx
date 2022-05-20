37995664d284840a12dce5545aa8d04c400d9db4
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-20 17:08:39 +0400

    style Bio component

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Bio = () => {
-	return <div>Bio</div>
+	return (
+		<div className='bio'>
+			<p className='bio__paragraph'>
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
+				Quisque volutpat mattis eros.
+			</p>
+		</div>
+	)
 }
 
 export default Bio
