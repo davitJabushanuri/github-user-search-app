fdd5099486b73ae3270be6b62791a952e143d30a
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-05-20 17:31:15 +0400

    style links component

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import locationIcon from '../assets/icon-location.svg'
+import linksIcon from '../assets/icon-website.svg'
+import twitterIcon from '../assets/icon-twitter.svg'
+import companyIcon from '../assets/icon-company.svg'
 
 const Links = () => {
-	return <div>Links</div>
+	return (
+		<div className='links'>
+			<div className='links__flex'>
+				<img src={locationIcon} alt='' className='links__flex__img' />
+				<h2 className='links__flex__header'>San Francisco</h2>
+			</div>
+			<div className='links__flex'>
+				<img src={linksIcon} alt='' className='links__flex__img' />
+				<h2 className='links__flex__header'>https://github.blog</h2>
+			</div>
+			<div className='links__flex'>
+				<img src={twitterIcon} alt='' className='links__flex__img' />
+				<h2 className='links__flex__header'>Not Available</h2>
+			</div>
+			<div className='links__flex'>
+				<img src={companyIcon} alt='' className='links__flex__img' />
+				<h2 className='links__flex__header'>@github</h2>
+			</div>
+		</div>
+	)
 }
 
 export default Links
